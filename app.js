0384c96d07141e108d2f6cda0d8b2f2b12b5348c
waterbear-tw <awdr563214789@gmail.com>
2023-05-10 22:41:03 +0800

    feat: add express-handlebars.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -11,7 +15,7 @@ db.on("error", () => console.log(" MongoDB connection error occurs!"));
 db.once("open", () => console.log("MongoDB connect successfully."));
 
 app.get("/", (req, res) => {
-  res.send("hi.");
+  res.render("index");
 });
 
 app.listen(3000, () => {
